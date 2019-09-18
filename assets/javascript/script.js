e08a58d7054dbc0555a3b0d2f737a455bedf8bc8
Ryan Goderis <rgoderis789@gmail.com>
2019-09-18 17:38:53 -0400

    renamed submit button id to kebabcase, saved tickmaster response requests to vars

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,8 +4,9 @@ var zip;
 $(document).ready(function(){
 
     // click listener on zipSubmit 
-    $("#zipSubmit").on("click", function(event){
-        event.preventDefault();
+    $("#events-btn").on("click", function(event){
+        // event.preventDefault();
+        console.log("you clicked a button")
         // sets value to zipcode_inline to zip
         zip = $("#zipcode_inline").val().trim();
     
@@ -18,14 +19,23 @@ $(document).ready(function(){
         url: queryURL,
         method: "GET"
     }).then(function(response){
-        for(var i = 0; i < 20; i++){
-            console.log("Event Name: " + response._embedded.events[i].name)
-            console.log("IMG URL: " + response._embedded.events[i].images[1].url)
-            console.log("Date: " + response._embedded.events[i].dates.start.localDate)
-            console.log("Start Time: " + response._embedded.events[i].dates.start.localTime)
-            console.log("Venue: "+ response._embedded.events[i]._embedded.venues[0].name);
-            console.log("Address: "+ response._embedded.events[i]._embedded.venues[0].address.line1  + " " + response._embedded.events[i]._embedded.venues[0].city.name+ ", " + response._embedded.events[i]._embedded.venues[0].state.name + " " + response._embedded.events[i]._embedded.venues[0].postalCode)
-            console.log("Buy tickets at: "+ response._embedded.events[i].url)
+        // loop through results
+        for(var i = 0; i < 5; i++){
+            var eventName = response._embedded.events[i].name;
+            var imgURL = response._embedded.events[i].images[1].url;
+            var date = response._embedded.events[i].dates.start.localDate;
+            var startTime = response._embedded.events[i].dates.start.localTime;
+            var venue = response._embedded.events[i]._embedded.venues[0].name;
+            var address = response._embedded.events[i]._embedded.venues[0].address.line1  + " " + response._embedded.events[i]._embedded.venues[0].city.name+ ", " + response._embedded.events[i]._embedded.venues[0].state.name + " " + response._embedded.events[i]._embedded.venues[0].postalCode
+            var tickmasterURL = response._embedded.events[i].url;
+
+            console.log(eventName)
+            console.log(imgURL)
+            console.log(date)
+            console.log(startTime)
+            console.log(venue)
+            console.log(address)
+            console.log(tickmasterURL)
         }
         console.log(response)
     })
